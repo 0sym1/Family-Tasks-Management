0b4945444701066cf8891374469978ec54e38169
phuc <dangvanphuc040404@gmail.com>
2026-03-25 09:08:30 +0700

    home ui

--- app/src/main/java/com/tngoc/familytaskapp/ui/home/HomeActivity.java
+++ app/src/main/java/com/tngoc/familytaskapp/ui/home/HomeActivity.java
@@ -2,11 +2,9 @@ package com.tngoc.familytaskapp.ui.home;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.navigation.NavController;
-import androidx.navigation.fragment.NavHostFragment;
-import androidx.navigation.ui.NavigationUI;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tngoc.familytaskapp.R;
@@ -14,21 +12,37 @@ import com.tngoc.familytaskapp.ui.auth.LoginActivity;
 
 public class HomeActivity extends AppCompatActivity {
 
-    private NavController navController;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.nav_host_fragment);
-
-        if (navHostFragment != null) {
-            navController = navHostFragment.getNavController();
-            BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
-            NavigationUI.setupWithNavController(bottomNav, navController);
-        }
+        // Set up BottomNavigationView
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        
+        // Set home as default active item
+        bottomNav.setSelectedItemId(R.id.homeFragment);
+        
+        // Handle navigation item selection
+        bottomNav.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.homeFragment) {
+                // Home - already here
+                return true;
+            } else if (item.getItemId() == R.id.taskFragment) {
+                Toast.makeText(this, "Nhiệm vụ", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (item.getItemId() == R.id.chatBotFragment) {
+                Toast.makeText(this, "Chat bot", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (item.getItemId() == R.id.notificationFragment) {
+                Toast.makeText(this, "Thông báo", Toast.LENGTH_SHORT).show();
+                return true;
+            } else if (item.getItemId() == R.id.profileFragment) {
+                Toast.makeText(this, "Cá nhân", Toast.LENGTH_SHORT).show();
+                return true;
+            }
+            return false;
+        });
     }
 
     public void logout() {
@@ -36,4 +50,6 @@ public class HomeActivity extends AppCompatActivity {
         finish();
     }
 }
+
+
 
